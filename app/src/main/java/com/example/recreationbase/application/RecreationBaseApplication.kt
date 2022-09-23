aa2cd6a8a3aa5fd786f295201f93a8d13681dd5e
Eganin <egorzaharin12@gmail.com>
2022-09-23 19:30:47 +0500

    add api

---
+++ app/src/main/java/com/example/recreationbase/application/RecreationBaseApplication.kt
@@ -0,0 +1,7 @@
+package com.example.recreationbase.application
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class RecreationBaseApplication : Application()
---
+++ app/src/main/java/com/example/recreationbase/data/remote/RecreationBaseApi.kt
@@ -0,0 +1,38 @@
+package com.example.recreationbase.data.remote
+
+import okhttp3.ResponseBody
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface RecreationBaseApi {
+
+    @GET("api/base-app/blog?id=117&format=card")
+    suspend fun getBlogs() : ResponseBody
+
+    @GET("api/base-app/blog-info?id=117")
+    suspend fun getDetailBlogInfo(
+        @Query("blog_id") blogId: Int
+    )
+
+    @GET("api/base-app/fun?id=117&type=food")
+    suspend fun getFoods()
+
+    @GET("api/base-app/rooms?id=117")
+    suspend fun getRooms()
+
+    @GET("api/base-app/fun?id=117&type=fun")
+    suspend fun getFun()
+
+    @GET("api/base-app/fun?id=117&type=child")
+    suspend fun getFunForChilds()
+
+    @GET("api/base-app/tours?id=117")
+    suspend fun getTours()
+
+    @GET("api/base-app/fun?id=117&type=place")
+    suspend fun getPlace()
+
+    companion object{
+        const val BASE_URL = "https://rsttur.ru/"
+    }
+}
